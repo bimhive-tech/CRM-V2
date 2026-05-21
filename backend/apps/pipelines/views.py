e2d128e0a4933830846bf561b29a4658b1e7bc52
Youssef Sami Yehia <youssef.sami01fl@outlook.com>
2026-05-21 11:31:39 +0300

    Permissions

--- backend/apps/pipelines/views.py
+++ backend/apps/pipelines/views.py
@@ -1,4 +1,5 @@
 from django.db import transaction
+from django.db.models import Q
 from rest_framework import generics, permissions, serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
